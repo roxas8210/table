9aa46473f1628939cc517c299cc1d75d9909de8b
ROXAS <roxas8210@163.com>
2017-07-06 16:47:13 +0800

    测试数据传递

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="root">
+    <p v-on:click="add">{{test}}</p>
+    <div class="mission">
+      <Mission v-bind:test="test"></Mission>
+    </div>
+  </div>
+</template>
+
+<script>
+import Mission from './components/Mission.vue'
+export default {
+  name: 'app',
+  components: {
+    Mission
+  },
+  data: function () {
+    return {
+      test: 'test'
+    }
+  },
+  methods: {
+    add: function () {
+      let theNum = this.test + '1';
+      this.test = theNum;
+    }
+  }
+}
+
+</script>
+
---
+++ src/components/Mission.vue
@@ -0,0 +1,43 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <td>公司名</td>
+                <td>语言</td>
+                <td>手机站</td>
+                <td>设计</td>
+                <td>价格</td>
+                <td>程序完成情况</td>
+                <td>资料上传情况</td>
+                <td>是否上传测试空间</td>
+                <td>测试空间网址</td>
+                <td>是否上传正式空间</td>
+                <td>网址</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-on:click="addTest">
+                <td>{{test}}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    name: 'mission',
+    props: ['test'],
+    data: function () {
+        return {
+            fatherTest: this.test
+        }
+    },
+    methods: {
+        addTest: function () {
+            let theNum = this.fatherTest + '1';
+            this.fatherTest = theNum;
+        }
+    }
+}
+</script>
+
