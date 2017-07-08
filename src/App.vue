afc0fc53cbc14199d13135e74333ab842898a727
ROXAS <roxas8210@163.com>
2017-07-08 14:54:01 +0800

    开始使用vue-router

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,16 @@
   <div class="root">
     <h1>做单记录</h1>
     <div class="mission">
-      <Mission></Mission>
+      <!--<Mission></Mission>-->
+      <router-link to="/"></router-link>
+      <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script>
 import Mission from './components/Mission.vue'
+
 export default {
   name: 'app',
   components: {
--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Mission from './components/Mission.vue'
+
+Vue.use(VueRouter);
+
+const routes = [{
+  path: '/',
+  component: Mission
+}];
+
+const router = new VueRouter({
+  routes: routes
+});
 
 var app = new Vue({
   el: '#app',
+  router: router,
   render:h => h(App)
 });
